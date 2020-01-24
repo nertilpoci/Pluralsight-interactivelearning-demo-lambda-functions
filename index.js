4306eef9f40f46d00f1eda386a6e8981b7f1ec04
nertil <nertilpoci@hotmail.com>
2020-01-24 15:40:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports.handler = async (event) => {
     return {
          "statusCode":200,
+         "body":`${event && event.queryStringParameters.firstName} ${event && event.queryStringParameters.lastName}`
     };
 };
  
